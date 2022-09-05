df85f928dcccee76ddaa6047a84c1cad488d6550
Edward Lopez-Ramos <lopez.edward@outlook.com>
2022-09-05 03:12:09 -0400

    feat: login endpoint operational

--- src/api/auth/auth.routes.ts
+++ src/api/auth/auth.routes.ts
@@ -6,6 +6,7 @@ import express from 'express';
 import { uuid } from 'uuidv4';
 import { generateTokens } from '../../utils/jwt';
 import { addRefreshTokenToWhitelist } from './auth.services';
+import bcrypt from 'bcrypt';
 
 const router = express.Router();
 
@@ -32,6 +33,37 @@ router.post('/register', async (req, res, next) => {
       accessToken,
       refreshToken,
     });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/login', async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400);
+      throw new Error('You must provide an email and password');
+    }
+
+    const existingUser = await findUserByEmail(email);
+
+    if (!existingUser) {
+      res.status(403);
+      throw new Error('Invalid login credentials');
+    }
+
+    const validPassword = await bcrypt.compare(password, existingUser.password);
+    if (!validPassword) {
+      res.status(403);
+      throw new Error('Invalid login credentials.');
+    }
+
+    const jti = uuid();
+    const { accessToken, refreshToken } = generateTokens(existingUser, jti);
+    await addRefreshTokenToWhitelist(jti, refreshToken, existingUser.id);
+
+    res.json({ accessToken, refreshToken });
   } catch (err) {
     next(err);
   }
